7270e5c4d49a43330962b858bf924afa8908314f
Srivathsav01 <srivathsav.m.2001@gmail.com>
2026-05-28 19:12:42 +0530

    first commit

---
+++ src/main/java/com/sri/miniqueue/MiniqueueApplication.java
@@ -0,0 +1,13 @@
+package com.sri.miniqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniqueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sri/miniqueue/MiniqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sri.miniqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
