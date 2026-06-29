9148c30c00869120f3500ee82f47685bb357737f
Srivathsav01 <srivathsav.m.2001@gmail.com>
2026-06-29 18:56:21 +0530

    Added retry mechanism

--- src/main/java/com/sri/miniqueue/scheduler/RedeliveryScheduler.java
+++ src/main/java/com/sri/miniqueue/scheduler/RedeliveryScheduler.java
@@ -10,6 +10,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -21,6 +23,9 @@ public class RedeliveryScheduler {
     @Value("${miniqueue.redelivery.timeout-minutes}")
     private int timeoutMinutes;
 
+    @Value("${miniqueue.redelivery.max-tries}")
+    private int retryCount;
+
     @Scheduled(fixedDelayString = "${miniqueue.redelivery.fixed-delay-ms}")
     public void resetUnackedMessages(){
         List<Message> messages = this.messageRepository.findStuckMessages(MessageStatus.UNACKED, LocalDateTime.now().minusMinutes(timeoutMinutes));
@@ -28,12 +33,22 @@ public class RedeliveryScheduler {
             log.info("No stuck messages found");
             return;
         }
+        AtomicInteger deadCount= new AtomicInteger(0);
         messages.forEach(msg->{
-            msg.setStatus(MessageStatus.PENDING);
-            msg.setUnackedAt(null);
-            msg.setConsumerId(null);
+            Integer retries = msg.getRetryCount()+1;
+            if(retries>=retryCount){
+                msg.setStatus(MessageStatus.DEAD);
+                deadCount.incrementAndGet();
+            }
+            else{
+                msg.setStatus(MessageStatus.PENDING);
+                msg.setUnackedAt(null);
+                msg.setConsumerId(null);
+            }
+            msg.setRetryCount(retries);
         });
-        log.info("Redelivering {} stuck messages", messages.size());
+        log.info("Redelivering {} stuck messages", messages.size()- deadCount.get());
+        log.warn("Messages moved to dead-letter queue: {}", deadCount.get());
         this.messageRepository.saveAll(messages);
     }
 }
